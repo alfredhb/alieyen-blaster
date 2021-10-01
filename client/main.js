3fe959ae23b7a79cbf347e39907bd555725411bc
Alfred Herbst <alfredhb@umich.edu>
2021-10-01 15:32:10 -0400

    Add Phaser Game
    Followed initial tutorial to inject phaser in, and added some methods to show how files talk to eachother

--- client/main.js
+++ client/main.js
@@ -1,22 +1,98 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import 'phaser';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+// Phaser Load
+function preload() {
+  console.log("preload");
+  this.load.setBaseURL('https://labs.phaser.io');
+
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('red', 'assets/particles/red.png');
+}
+
+function create() {
+  console.log("create");
+  this.add.image(400, 300, 'sky');
+
+  var particles = this.add.particles('red');
+
+  var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+  });
+
+  var logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create
+  }
+};
+
+var game = new Phaser.Game(config);
+
+// Other Helper Functions
+function loadHighscore() {
+  let highscore = 0;
+  let scoreHolder = "emtpy";
+
+  Meteor.call("loadHighscore", (err, res) => {
+    if (typeof(res) != "string") {
+      return;
+    }
+
+    // Update div
+    let scoreData = res.split(" ");
+    scoreHolder = scoreData[0];
+    highscore = scoreData[1];
+
+    document.getElementById("scoreHolder").innerHTML = scoreHolder;
+    document.getElementById('highscore').innerHTML = highscore;
+  });
+}
+
+function saveHighscore() {
+  console.log("Unimplemented!");
+}
+
+Template.score.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  loadHighscore()
 });
 
-Template.hello.helpers({
+Template.score.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.score.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import 'fs';
+
+const fs = require("fs")
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+  loadHighscore() {
+    var highscore = fs.readFileSync("../../../../../savedata/counter_highscores.txt");
+    var score = highscore.toString();
+  
+    return score;
+  },
 });
