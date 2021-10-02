aaf0c016da3e90b91baab0c1cb427cbdb98c2235
Alfred Herbst <alfredhb@umich.edu>
2021-10-02 17:29:11 -0400

    Basic Main Menu

--- client/scenes/menus/main.js
+++ client/scenes/menus/main.js
@@ -1,15 +1,72 @@
 import Phaser from 'phaser';
 
 export default class Scene1 extends Phaser.Scene {
+    
     constructor() {
         super('mainMenu')
     }
 
-    create() {
-        this.add.text(20, 20, 'Loading..')
+    init() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
-        // setTimeout(() => {
-        //     this.scene.start('game', 2000)
-        // })
+    create() {
+        const { width, height } = this.scale;
+
+        this.add.text(width * 0.22, height * 0.1, 'Ali-eye-n Blaster 3000', {
+            fontFamily: "Lekton",
+            fontSize: "50px",
+            strokeThickness: 0,
+        });
+        
+        this.initButtons(width, height);
+    }
+
+    initButtons(width, height) {
+        // Singlelplayer button
+        const spButton = this.add.image(width * 0.3, height * 0.6, 'panel')
+            .setDisplaySize(250, 350);
+        
+        this.add.text(spButton.x, spButton.y, 'Singlelplayer')
+            .setOrigin(0.5);
+    
+        // Multiplayer button
+        const mpButton = this.add.image(width * 0.7, height * 0.6, 'white')
+            .setDisplaySize(250, 350);
+    
+        this.add.text(mpButton.x, mpButton.y, 'Multiplayer')
+            .setOrigin(0.5);
+    
+        // Quit button
+        const qButton = this.add.image(width * 0.95, height * 0.93, 'white')
+            .setDisplaySize(50, 50);
+    
+        this.add.text(qButton.x, qButton.y, 'Quit')
+            .setOrigin(0.5);
+
+        this.buttons = [spButton, mpButton, qButton]
+
+        // Create Interactives
+        for (let button of this.buttons) {
+            button.setInteractive();
+
+            button.on('pointerover', () => {
+                button.setTint(0x7878ff);
+            })
+            button.on('pointerout', () => {
+                button.clearTint();
+            })
+        }
+
+        // Set action for specific buttons
+        spButton.on('pointerup', () => {
+            this.scene.start('gamemodeMenu');
+        });
+        mpButton.on('pointerup', () => {
+            console.log('Unimplemented');
+        });
+        qButton.on('pointerup', () => {
+            console.log('Unimplemented');
+        });
     }
 }
