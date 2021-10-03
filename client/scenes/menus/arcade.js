cae9f7d27eba976a414caea2260a900392fcfb28
Alfred Herbst <alfredhb@umich.edu>
2021-10-03 15:29:22 -0400

    Update L64 to use correct scene name

--- client/scenes/menus/arcade.js
+++ client/scenes/menus/arcade.js
@@ -61,7 +61,7 @@ export default class MenuScene3 extends Phaser.Scene {
             console.log('Unimplemented');
         });
         qButton.on('pointerup', () => {
-            this.scene.start('gamemdeMenu');
+            this.scene.start('gamemodeMenu');
         });
     }
 }
