99486603fcc731bba307fa887fcd72c99b817dc7
Alfred Herbst <alfredhb@umich.edu>
2021-10-03 15:37:56 -0400

    Comment out old assets

--- client/scenes/data/initial_load.js
+++ client/scenes/data/initial_load.js
@@ -7,14 +7,18 @@ export default class DataScene1 extends Phaser.Scene {
 
     preload() {
         this.assetLocations = [
-            {key: "big-button", path: "buttons/menu-sample-big.png"},
-            {key: "small-button", path: "buttons/menu-sample-small.png"}
+            // {key: "big-button", path: "buttons/menu-sample-big.png"},
+            // {key: "small-button", path: "buttons/menu-sample-small.png"}
         ]
         this.assetsLoaded = 0;
     }
 
     create() {
         this.add.text(20, 20, 'Loading...')
+
+        if (this.assetLocations.length == 0) {
+            this.scene.start('mainMenu');
+        }
 
         // load all assets
         
