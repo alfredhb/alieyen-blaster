07e824bdcd8b304c15b2197bb41268d164782c3e
Alfred Herbst <alfredhb@umich.edu>
2021-10-03 17:32:02 -0400

    Add Sound Loading

--- client/scenes/data/initial_load.js
+++ client/scenes/data/initial_load.js
@@ -6,7 +6,9 @@ export default class DataScene1 extends Phaser.Scene {
     }
 
     preload() {
-        this.assetLocations = [
+        this.load.setBaseURL('https://labs.phaser.io');
+
+        this.imageAssets = [
             // Backgrounds
             {key: "arcade-bg", path: "background/background3.png"},
             {key: "space-bg", path: "background/background1.png"},
@@ -16,28 +18,29 @@ export default class DataScene1 extends Phaser.Scene {
             {key: "gameslot-button", path: "buttons/game_slot.png"},
             {key: "story-button", path: "buttons/story_no_text.png"}
         ]
-        this.assetsLoaded = 0;
-
-        if (this.assetLocations.length == 0) {
-            this.scene.start('startMenu');
-        }
+        this.soundAssets = [
+            {key: "menu-click", path: "assets/audio/kyobi/wavs/menuClick.wav"},
+        ]
+        this.imagesLoaded = 0;
 
         // load all assets
         // TODO: Parallelization?
         
         // Load images
-        // // this.textures.addBase64('big-button', bigButton);
-        // this.load.image('small-button', 'assets/particles/red.png');
-        for (let asset of this.assetLocations) {
+        if (this.imageAssets.length == 0) {
+            this.scene.start('startMenu');
+        }
+
+        for (let asset of this.imageAssets) {
             this.textures.once('addtexture', () => {
-                this.assetsLoaded += 1;
+                this.imagesLoaded += 1;
 
                 // show asset has loaded.
-                this.add.text(20, 20 + 10 * this.assetsLoaded, 'added texture: ' + asset.key)
-                this.add.image(80, 20 + 10 * this.assetsLoaded, asset.key).setDisplaySize(20, 20);
+                this.add.text(20, 20 + 10 * this.imagesLoaded, 'added texture: ' + asset.key)
+                this.add.image(80, 20 + 10 * this.imagesLoaded, asset.key).setDisplaySize(20, 20);
                 console.log("added texture: ", asset.key)
 
-                if (this.assetsLoaded == this.assetLocations.length) {
+                if (this.imagesLoaded == this.imageAssets.length) {
                     setTimeout(() => {
                         this.scene.start('startMenu');
                     }, 2000);
@@ -54,5 +57,14 @@ export default class DataScene1 extends Phaser.Scene {
                 this.textures.addBase64(asset.key, data);
             });
         }
+
+        // Load Sounds
+        for (let asset of this.soundAssets) {
+            this.load.audio(asset.key, asset.path);
+        }
+    }
+
+    create() {
+        this.add.text(20, 20, 'Loading...')
     }
 }
