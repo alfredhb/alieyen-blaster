3d0323f53c3427d1970e5999c7ac5e4a1f8c5d39
Alfred Herbst <alfredhb@umich.edu>
2021-10-03 16:28:08 -0400

    Style Arcade menu

--- client/scenes/menus/arcade.js
+++ client/scenes/menus/arcade.js
@@ -8,6 +8,10 @@ export default class MenuScene3 extends Phaser.Scene {
     create() {
         const { width, height } = this.scale;
 
+        // Background
+        this.add.image(width * 0.5, height * 0.5, 'arcade-bg').setDisplaySize(width, height)
+
+        // Title
         this.add.text(width * 0.4, height * 0.1, 'Arcade', {
             fontFamily: "Lekton",
             fontSize: "50px",
@@ -18,17 +22,26 @@ export default class MenuScene3 extends Phaser.Scene {
     }
 
     initButtons(width, height) {
-        // Singlelplayer button
-        const tiButton = this.add.image(width * 0.3, height * 0.6, 'big-button').setDisplaySize(200, 200);
-        this.add.text(tiButton.x, tiButton.y, 'Timed', {
-            color: "#000000"
-        }).setOrigin(0.5);
+        let textStyle = {
+            fontSize: "50px",
+            color: "#FFFFFF",
+        }
+
+        // Timed button
+        const tiButton = this.add.image(width * 0.25, height * 0.35, 'big-button').setDisplaySize(width * .35, height * .25);
+        this.add.text(tiButton.x, tiButton.y, 'Timed', textStyle).setOrigin(0.5);
     
-        // Multiplayer button
-        const enButton = this.add.image(width * 0.7, height * 0.6, 'big-button').setDisplaySize(200, 200);
-        this.add.text(enButton.x, enButton.y, 'Endless', {
-            color: "#000000"
-        }).setOrigin(0.5);
+        // Endless button
+        const enButton = this.add.image(width * 0.75, height * 0.35, 'big-button').setDisplaySize(width * .35, height * .25);
+        this.add.text(enButton.x, enButton.y, 'Endless', textStyle).setOrigin(0.5);
+
+        // Lives Button
+        const liButton = this.add.image(width * 0.25, height * 0.7, 'big-button').setDisplaySize(width * .35, height * .25);
+        this.add.text(liButton.x, liButton.y, 'Timed', textStyle).setOrigin(0.5);
+    
+        // Gauntlet button
+        const gaButton = this.add.image(width * 0.75, height * 0.7, 'big-button').setDisplaySize(width * .35, height * .25);
+        this.add.text(gaButton.x, gaButton.y, 'Endless', textStyle).setOrigin(0.5);
     
         // Quit button
         const qButton = this.add.image(width * 0.95, height * 0.93, 'small-button').setDisplaySize(50, 50);
@@ -39,7 +52,7 @@ export default class MenuScene3 extends Phaser.Scene {
 			stroke: '#FF0000',
         }).setOrigin(0.5);
 
-        this.buttons = [tiButton, enButton, qButton]
+        this.buttons = [tiButton, enButton, liButton, gaButton, qButton]
         // Create Interactives
         for (let button of this.buttons) {
             button.setInteractive();
