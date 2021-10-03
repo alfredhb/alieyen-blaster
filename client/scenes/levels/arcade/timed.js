6cb2294bf9b2576bbd515d8167e18ba48d7a63ac
Alfred Herbst <alfredhb@umich.edu>
2021-10-03 18:08:07 -0400

    Beginning Timed Level
    Added initial hud workings

--- client/scenes/levels/arcade/timed.js
+++ client/scenes/levels/arcade/timed.js
@@ -5,11 +5,56 @@ export default class ArcadeScene1 extends Phaser.Scene {
         super('timedArcade')
     }
 
+    preload() {
+        // Load Sounds
+        this.menuSounds = {
+            menuClick: this.sound.add('menu-click', { loop: false, volume: .5}),
+        }
+    }
+
     create() {
+        const { width, height } = this.scale;
         this.add.text(20, 20, 'Loading..')
 
-        // setTimeout(() => {
-        //     this.scene.start('game', 2000)
-        // })
+        // Init Graphics (background, hud, quit)
+        this.initHud(width, height);
+
+        // Init Tutorial Screen (overlay)
+
+        // Add Game logic
+    }
+
+    initHud(width, height) {
+        // Add Background
+        this.add.image(width * 0.5, height * 0.5, 'space-bg').setDisplaySize(width, height)
+
+        // Add Cockpit
+
+        // Add Time
+
+        // Add Score
+
+        // Quit button
+        const qButton = this.add.image(width * 0.95, height * 0.93, 'small-button').setDisplaySize(50, 50);
+        const qText = this.add.text(qButton.x, qButton.y, 'X', {
+            color: "#FF0000",
+            fontSize: "50px",
+            strokeThickness: 3,
+			stroke: '#FF0000',
+        }).setOrigin(0.5);
+        const qSound = this.menuSounds.menuClick;
+
+        qButton.setInteractive()
+        .on('pointerover', () => {
+            qButton.setTint(0xFF0000);
+            qText.setTint(0xFFF);
+            // buttonObj.sound.play(); // Play the saved sound
+        }).on('pointerout', () => {
+            qButton.clearTint();
+            qText.clearTint();
+        }).on('pointerup', () => {
+            this.menuSounds.menuClick.play();
+            qSound.play();
+        })
     }
 }
