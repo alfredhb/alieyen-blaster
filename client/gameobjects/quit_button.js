f2150f532fab28535cedcb0559c8ded7917f1386
Alfred Herbst <alfredhb@umich.edu>
2021-10-10 16:26:34 -0400

    Add quit sound

--- client/gameobjects/quit_button.js
+++ client/gameobjects/quit_button.js
@@ -1,10 +1,18 @@
 import Phaser from "phaser";
+import Constants from "../lib/constants";
 
 export default class QuitButton extends Phaser.GameObjects.Group {
+    /**
+     * 
+     * @param {Phaser.Scene} scene 
+     * @param {{execFunc: function, backMenu: string, data: object}} config 
+     */
     constructor(scene, config) {
         super(scene);
 
         const { width, height } = scene.scale;
+        this.constants = new Constants();
+
         const button = scene.add.image(width * 0.95, height * 0.93, '__WHITE');
         button.setDisplaySize(width * 0.05, width * 0.05);
         button.setDepth(10);
@@ -16,12 +24,17 @@ export default class QuitButton extends Phaser.GameObjects.Group {
         }).setOrigin(0.5);
         text.setDepth(11);
         const clickSound = scene.sound.add('menu-click', { loop: false, volume : 0.5});
+        const hoverSound = scene.sound.add('quit', { loop: false });
 
         // Add interactives
         button.setInteractive();
         button.on('pointerover', () => {
             button.setTint(0xFF0000);
             text.setTint(0xFFF);
+
+            if (!hoverSound.isPlaying) {
+                hoverSound.play();
+            }
         });
         button.on('pointerout', () => {
             button.clearTint();
