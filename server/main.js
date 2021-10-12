5969f606dfbb242097dc3eceae50c0a6444a20f0
Alfred Herbst <alfredhb@umich.edu>
2021-10-11 22:12:52 -0400

    Add localdev checker

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import 'fs';
 
 const fs = require("fs")
+const cwd_arr = fs.realpathSync(process.cwd()).split("\\");
+const isLocalDev = cwd_arr[cwd_arr.length - 3] == "build";
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -17,9 +19,16 @@ Meteor.methods({
 
   // Loads asset from ./data/assets/{path} and returns a base64 object of it
   loadImageAsset(path) {
+    let pathPrefix = "";
+    if (isLocalDev) {
+      pathPrefix = "../../../../../public/";
+    } else {
+      pathPrefix = "../web.browser/";
+    }
+
     try {
       console.log(process.cwd());
-      var asset = fs.readFileSync("../../../../../public/assets/" + path);
+      var asset = fs.readFileSync(pathPrefix + "assets/" + path);
 
       return asset.toString('base64');
     } catch {
@@ -32,9 +41,16 @@ Meteor.methods({
    * @returns {string} base64 of asset
    */
   loadSoundAsset(path) {
+    let pathPrefix = "";
+    if (isLocalDev) {
+      pathPrefix = "../../../../../public/";
+    } else {
+      pathPrefix = "../web.browser/app/";
+    }
+
     try {
       console.log(process.cwd());
-      var asset = fs.readFileSync("../../../../../public/sounds/" + path);
+      var asset = fs.readFileSync(pathPrefix + "sounds/" + path);
 
       return asset.toString('base64');
     } catch (e) {
