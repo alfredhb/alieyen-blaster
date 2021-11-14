d3d2a0d2187b5d74179a59f75ef090423548170d
Alfred Herbst <alfredhb@umich.edu>
2021-11-14 00:15:24 -0500

    Update hitbox and projectile killing
    Only kill a projectile if its not null, then also destroy it

--- client/gameobjects/alien_grunt.js
+++ client/gameobjects/alien_grunt.js
@@ -17,7 +17,7 @@ export default class AlienGrunt extends Alien {
         this.setInteractive();
         this.setPosition(width + 50, height + 50);
         this.setDisplaySize(width * 0.03, height * 0.05);
-        this.body.setSize(this.displayWidth * 1.3, this.displayHeight * 2);
+        this.body.setSize(this.displayWidth * 0.7, this.displayHeight * 1.5);
         // this.setOrigin(0.5);
         this.body.setOffset(this.displayWidth * 1.1, this.displayHeight)
 
@@ -157,8 +157,11 @@ export default class AlienGrunt extends Alien {
         this.deadVal = true;
 
         // let projectile handle killed alien
-        this.projectile?.alienKilled();
-
+        if (this.projectile) {
+            this.projectile.alienKilled();
+            this.projectile.destroy();
+        }
+        
         // Stop any charge sound and play animation
         this.play('explode', { loop: false, volume: 0.25 });
         this.on('animationcomplete', () => {
